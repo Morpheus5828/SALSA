dabb9d3011585e63081471ba127c2c2b49ab8a0f
Marius THORRE Portable <marius.thorre@etu.univ-amu.fr>
2023-02-21 23:00:19 +0100

    Create REAME file, score image recognition part done

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1>SALSA Project</h1>
+
+
+![logo_amu.png](logo_amu.png)
+---
+
+Marius THORRE <br>
+Chloé BUTTIGIEF <br>
+Warren AHAMADA
+<h2><i>Does image has an Ocean or a Sea ?</i></h2>
+<p>Try to give an answer using machine learning, an Artifical Intelligence branch.
+<br> It exis to different type of learning: <br>
+<p>&rarr; supervised learning: </p>
+<p>&emsp;&emsp;The AI has learnt to recognize something using dataset during learning period. (here we are)</p>
+<p>&rarr; not supervised learning: </p>  
+<p>&emsp;&emsp;The AI doesn't need learning period to predic something</p>
+<br> Our dataset contains 400 pictures between sea/ocean and other one.
+
+
+<h2><u>Part One:</u> Image detection</h2>
+<p>In a first time, we decided to create an algorithm to detect <a target="_blank" href="https://docs.opencv.org/3.4/d7/d66/tutorial_feature_detection.html">keypoints</a> in a random image, calling source and an other one calling destination.
+If keypoint detected in source are already in destination, then it's the samed image ! Let's start. </p>
+
+<h2><u>Part Two:</u> Image processing</h2>
+chloé part ....
+
--- image_detection/image_compare.py
+++ image_detection/image_compare.py
@@ -8,16 +8,6 @@ import numpy as np
 from threading import Thread
 from matplotlib import pyplot as plt
 
-sea_ocean = os.listdir("../dataset/sea_ocean/")
-img = sea_ocean[0]  # 838s.jpg
-
-img = cv2.imread("../dataset/sea_ocean/" + img)
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-gray = np.float32(gray)
-
-list_distance = []
-
 
 ############################# class
 
@@ -40,8 +30,10 @@ def get_keypoint(kp):
     return np.asarray([key_point.pt for key_point in kp])
 
 
-def detect_keypoint(n, image):
-    return cv2.FastFeatureDetector_create(n).detect(image, None)
+def detect_keypoint(image):
+    fast = cv2.FastFeatureDetector_create()
+    fast.setNonmaxSuppression(0)
+    return fast.detect(image, None)
 
 
 def get_distance(x1, y1, x2, y2):
@@ -84,50 +76,59 @@ def display_images_with_score(img1, img2, score):
 def get_cv2_img(path):
     return cv2.imread(path)
 
-def evaluate(kp1, kp2):
-    counter = 0
-    for i in kp1:
-        if i in kp2:
-            counter += 1
-    print("size: ", len(kp1))
-    print("size: ", len(kp2))
-    print("counter:", counter)
-    '''common = np.intersect1d(kp1, kp2).size
-    print("common: ", common)'''
-    return (100 * counter) // len(kp2)
 
+def intersection(a, b):
+    nrows, ncols = a.shape
+    dtype = {'names': ['f{}'.format(i) for i in range(ncols)],
+             'formats': ncols * [a.dtype]}
+
+    return np.intersect1d(a.view(dtype), b.view(dtype))
+
+
+def evaluate(kp1, kp2):
+    try:
+        '''print(len(kp1))
+        print(len(kp2))
+        print("size: ", (100 * len(common)) // len(kp2))'''
+        common = intersection(kp1, kp2)
+        return (100 * len(common)) / len(kp2)
+    except ZeroDivisionError:
+        print("Error: zero division")
+
+
+'''def display_kp(path):
+    img = cv2.imread(path)
+    fast = cv2.FastFeatureDetector_create()
+    kp = fast.detect(img, None)
+    img = cv2.drawKeypoints(img, kp, None, color=(255, 0, 0))
+    cv2.imshow(img)'''
+
+
+#display_kp("../dataset/sea_ocean/838s.jpg")
 
 def compare(img_source, img_dest):
-    start = time.time()
-
     # 0. convert image to cv2 image
     img_source = get_cv2_img(img_source)
     img_dest = get_cv2_img(img_dest)
 
     # 1. extract kp from img
-    img_source_kp = get_keypoint(detect_keypoint(50, img_source))
+    img_source_kp = get_keypoint(detect_keypoint(img_source))
 
     # 2. extract kp from other random image in repo
-    img_dest_kp = get_keypoint(detect_keypoint(50, img_dest))
+    img_dest_kp = get_keypoint(detect_keypoint(img_dest))
 
     # 3. evaluation
     evaluation = evaluate(img_source_kp, img_dest_kp)
 
-    print("Evaluation: ", evaluation, "%")
+    #print("Evaluation: ", evaluation, "%")
 
-    display_images_with_score(img, img, evaluation)
+    display_images_with_score(img_source, img_dest, evaluation)
 
-    end = time.time()
-    print("\n Execution time:", end - start)
-
+    return evaluation
 
 compare("../dataset/sea_ocean/838s.jpg", "../dataset/sea_ocean/838s.jpg")
 
+'''cv2.drawKeypoints(img,kp, None, flags=0)
 
-
-
-
-# img2 = cv2.drawKeypoints(img, kp, None, flags=0)
-
-# cv2.imshow('corne', img2)
+cv2.imshow('corne', img2)'''
 # cv2.waitKey(0)
